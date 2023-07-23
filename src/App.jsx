45c670f2be7d2ab813fb72fca41863a97f06b77c
jeffrey <jeffrey@lizard.global>
2023-07-23 13:35:00 +0100

    correct basename

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Navbar from "./layouts/Navbar";
 
 function App() {
   return (
-    <Router basename="https://my-pisces.vercel.app/">
+    <Router basename="/my-pisces">
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
