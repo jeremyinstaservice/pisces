c65edcd25e652f81fda19aaf205bb49a4ec8c199
jeffrey <jeffrey@lizard.global>
2023-07-23 13:27:59 +0100

    update fix for not found error

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Navbar from "./layouts/Navbar";
 
 function App() {
   return (
-    <Router basename="/myPisces/">
+    <Router basename="./">
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
